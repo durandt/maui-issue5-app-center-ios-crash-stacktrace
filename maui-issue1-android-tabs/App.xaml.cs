ed42105fb22412241a53090132147e9732fc19ee
Thibault Durand <thibault.durand@liu.se>
2022-12-19 14:22:29 +0100

    Project created from template

---
+++ maui-issue1-android-tabs/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace maui_issue1_android_tabs;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ maui-issue1-android-tabs/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace maui_issue1_android_tabs;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
